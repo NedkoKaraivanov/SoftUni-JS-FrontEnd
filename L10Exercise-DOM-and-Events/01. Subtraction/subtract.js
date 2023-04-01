5b617dc4544414530c2443ade23c26ba147c11fc
Nedko Karaivanov <nedko2016@gmail.com>
2023-04-01 12:38:23 +0300

    Exercise DOM and Events

--- L10Exercise-DOM-and-Events/01. Subtraction/subtract.js
+++ L10Exercise-DOM-and-Events/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstInput = document.getElementById('firstNumber');
+    const secondInput = document.getElementById('secondNumber');
+    const result = document.getElementById('result');
+
+    let firstNum = Number(firstInput.value);
+    let secondNum = Number(secondInput.value);
+    let resultAfterSubtract = firstNum - secondNum;
+
+    result.textContent = resultAfterSubtract;
 }
--- L10Exercise-DOM-and-Events/03. Accordion/accordion.js
+++ L10Exercise-DOM-and-Events/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const btnElement = document.getElementsByClassName('button')[0];
+    const extraDiv = document.getElementById('extra');
+
+    if (btnElement.textContent === 'More') {
+        extraDiv.style.display = 'block';
+        btnElement.textContent = 'Less';
+    } else {
+        extraDiv.style.display = 'none';
+        btnElement.textContent = 'More';
+    }
 }
