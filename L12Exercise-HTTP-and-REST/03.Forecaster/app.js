6a96e53b8a71569680763fd853ea621f0ef9deea
Nedko Karaivanov <nedko2016@gmail.com>
2023-04-06 22:48:16 +0300

    Update app.js

--- L12Exercise-HTTP-and-REST/03.Forecaster/app.js
+++ L12Exercise-HTTP-and-REST/03.Forecaster/app.js
@@ -9,46 +9,62 @@ function attachEvents() {
     const divUpcoming = document.getElementById('upcoming');
     const degreesSymbol = '&#176';
     let currentLocation = null;
+    const divForecasts = document.createElement('div');
+    const divForecastInfo = document.createElement('div');
+    divForecasts.setAttribute('class', 'forecasts');
+    divForecastInfo.setAttribute('class', 'forecast-info');
+    let labels = Array.from(document.getElementsByClassName('label'));
+    let firstLabel = labels[0];
+    let secondLabel = labels[1];
 
     getWeatherBtn.addEventListener('click', loadLocationsHandler);
 
     function loadLocationsHandler() {
+
+        divForecast.style.display = 'block';
         fetch(LOCATIONS_URL)
             .then((res) => res.json())
             .then((locationsArr) => {
                 let userInputValue = userInput.value;
+                let isFound = false;
                 for (const location of locationsArr) {
                     if (location.name === userInputValue) {
                         currentLocation = location;
-                        
-                        divForecast.style.display = 'block';
+                        isFound = true;
                         loadCurrentLocationData();
                         loadUpcomingLocationData();
                     }
                 }
+                if (!isFound) {
+                    firstLabel.textContent = 'Error';
+                    secondLabel.textContent = 'Error';
+                    divForecasts.innerHTML = '';
+                    divForecastInfo.innerHTML = '';
+                }
             })
             .catch((err) => {
+                divForecast.innerHTML = '';
                 divForecast.textContent = err.message;
             })
     }
 
     function loadCurrentLocationData() {
+        divForecasts.innerHTML = '';
+        divCurrent.appendChild(divForecasts);
+        firstLabel.textContent = 'Current conditions';
 
         fetch(`${CURRENT_FORECAST_URL}${currentLocation.code}`)
             .then((res) => res.json())
             .then((data) => {
-                const {name, forecast} = data;
-                const {condition, high, low} = forecast;
-                const symbol = getConditionSymbol(condition);   
-                let divForecasts = document.createElement('div');
+                const { name, forecast } = data;
+                const { condition, high, low } = forecast;
+                const symbol = getConditionSymbol(condition);
                 let spanConditionSymbol = document.createElement('span');
                 let spanCondition = document.createElement('span');
                 let spanTown = document.createElement('span');
                 let spanTemperature = document.createElement('span');
                 let spanWeather = document.createElement('span');
                 spanCondition.setAttribute('class', 'condition');
-                divForecasts.setAttribute('class', 'forecasts');
-                divCurrent.appendChild(divForecasts);
                 spanConditionSymbol.setAttribute('class', 'condition symbol');
                 spanConditionSymbol.innerHTML = symbol;
                 divForecasts.appendChild(spanConditionSymbol);
@@ -64,28 +80,29 @@ function attachEvents() {
                 spanCondition.appendChild(spanWeather);
             })
             .catch((err) => {
+                divForecast.innerHTML = '';
                 divForecast.textContent = err.message;
             });
     }
 
     function loadUpcomingLocationData() {
-        const divForecast = document.createElement('div');
-        divForecast.setAttribute('class', 'forecast-info');
-        divUpcoming.appendChild(divForecast);
+        divForecastInfo.innerHTML = '';
+        divUpcoming.appendChild(divForecastInfo);
+        secondLabel.textContent = 'Three-day forecast';
 
         fetch(`${UPCOMING_FORECAST_URL}${currentLocation.code}`)
             .then((res) => res.json())
             .then((data) => {
-                const {name, forecast} = data;
+                const { name, forecast } = data;
                 for (const day of forecast) {
-                    const {condition, high, low} = day;
+                    const { condition, high, low } = day;
                     let conditionSymbol = getConditionSymbol(condition);
                     let spanUpcoming = document.createElement('span');
                     let spanSymbol = document.createElement('span');
                     let spanTemperature = document.createElement('span');
                     let spanWeather = document.createElement('span');
                     spanUpcoming.setAttribute('class', 'upcoming');
-                    divForecast.appendChild(spanUpcoming);
+                    divForecastInfo.appendChild(spanUpcoming);
                     spanSymbol.setAttribute('class', 'symbol');
                     spanSymbol.innerHTML = conditionSymbol;
                     spanTemperature.setAttribute('class', 'forecast-data');
